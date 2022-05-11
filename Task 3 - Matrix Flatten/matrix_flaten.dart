76474761d250018ce13639ee90b654f002afc5ba
Abdelrahman Osama <abdo.messi54@gmail.com>
2022-05-11 17:42:05 +0200

    Index Function

--- Task 3 - Matrix Flatten/matrix_flaten.dart
+++ Task 3 - Matrix Flatten/matrix_flaten.dart
@@ -1,10 +1,21 @@
 import 'dart:io';
 
+//Function to flatten the matrix into 1D list
 List matrix_flatten(Iterable<dynamic> list) => [
       for (var item in list)
         if (item is! Iterable) item else ...matrix_flatten(item)
     ];
 
+int index3D_to_index1D(List list3D, var i, var j, var k) {
+  var n = list3D.length;
+  var m = list3D[0].length;
+  var p = list3D[0][0].length;
+  if (i >= n || j >= m || k >= p) {
+    print("Index out of range");
+    return -1;
+  }
+  return (i * m * p) + (j * p) + k;
+}
 
 void main(List<String> args) {
   List lis = [
@@ -19,5 +30,8 @@ void main(List<String> args) {
       [5, 6]
     ]
   ];
+
   print(matrix_flatten(lis));
+  print(index3D_to_index1D(lis, 0, 1, 2));
+  print(matrix_flatten(lis)[index3D_to_index1D(lis, 0, 1, 2)]);
 }
--- Task 3 - Matrix Flatten/matrix_flatten.py
+++ Task 3 - Matrix Flatten/matrix_flatten.py
@@ -1,25 +1,29 @@
+#Function to flatten the matrix into 1D list
 def flatten(l):
-    """
-    flat=[]
-    for item in l:
-        for c in item:
-            for x in c:
-                flat.append(x);
-    return flat
-    """
-
-    return [x 
+    return [
+            x 
             for item in l
             for c in item
             for x in c
-            ]
+        ]
     
 
+#Function to convert the i,j,k index into 1D list index
+def index3D_to_index1D (list3D , i , j , k) :
+    n = len(list3D)
+    m = len(list3D[0])
+    p = len(list3D[0][0])
+    if (i >= n or j >= m or k >= p) :
+        print("Index out of range")
+        return -1
+    return (i*m*p)+(j*p)+k
 
-
+#Verfication
 lis = [
     [[1,2],[3,4],[5,6]],
     [[1,2],[3,4],[5,6]]
 ]
 
-print(flatten(lis))
+list1D = flatten(lis)
+print (lis[1][2][0])
+print(list1D[index3D_to_index1D(lis,1,2,0)])
